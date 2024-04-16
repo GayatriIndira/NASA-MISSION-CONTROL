238c82e29b5cccb6d799c22cc396243b8864c2dc
GayatriIndira <cgayatri@indiratrade.com>
2024-04-16 16:48:51 +0530

    first commit

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,41 +1,41 @@
-// const {
-//     getAllLaunches,
-//     addNewLaunch,
-// } = require('../../models/launches.models');
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.models');
 
-// function httpGetAllLaunches(req, resp) {
-//     return resp.status(200).json(getAllLaunches);
-// }
-
-// function httpAddNewLaunch(req, resp) {
-//     const launch = req.body;
-//     launch.launchDate = new Date(launch.launchDate);
-
-//     addNewLaunch(launch);
-//     return resp.status(201).json(launch);
-// }
-
-// module.exports = {
-//     httpAddNewLaunch,
-//     httpGetAllLaunches,
-// }
-
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.models');
-
-function httpGetAllLaunches(req, resp) {
+function httpGetAllLaunches(req, res) {
     const launches = getAllLaunches(); // Call the function to get the array of launches
-    return resp.status(200).json(launches); // Return the launches array as JSON
+    return res.status(200).json(launches); // Return the launches array as JSON
 }
 
-function httpAddNewLaunch(req, resp) {
+function httpAddNewLaunch(req, res) {
     const launch = req.body;
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch property'
+        });
+    }
     launch.launchDate = new Date(launch.launchDate);
-
+    if(isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid Launch Date',
+        })
+    }
     addNewLaunch(launch);
     return resp.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    if(!existsLaunchWithId(launchId)) {
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
+    
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
 module.exports = {
     httpAddNewLaunch,
     httpGetAllLaunches,
+    httpAbortLaunch,
 };
