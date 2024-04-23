202d6f05d96c39d3e428ec8cf9aab8e2ee1bd45e
GayatriIndira <cgayatri@indiratrade.com>
2024-04-23 11:54:28 +0530

    build

--- nasa-front-end/src/App.js
+++ nasa-front-end/src/App.js
@@ -1,30 +1,22 @@
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
-import {
-  Arwes,
-  SoundsProvider,
-  ThemeProvider,
-  createSounds,
-  createTheme,
-} from "arwes";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Arwes, SoundsProvider, createSounds, createTheme } from "arwes";
 
 import AppLayout from "./pages/AppLayout";
+import { resources, sounds } from "./settings";
 
-import { theme, resources, sounds } from "./settings";
+const theme = createTheme(); // Assuming you're creating a theme object
 
 const App = () => {
-  return <ThemeProvider theme={createTheme(theme)}>
-    <SoundsProvider sounds={createSounds(sounds)}>
-      <Arwes animate background={resources.background.large} pattern={resources.pattern}>
-        {anim => (
-          <Router>
-            <AppLayout show={anim.entered} />
-          </Router>
-        )}
-      </Arwes>
-    </SoundsProvider>
-  </ThemeProvider>;
+  return (
+    <Arwes animate background={resources.background.large} pattern={resources.pattern}>
+      {(anim) => (
+        <Router>
+          <AppLayout show={anim.entered} />
+        </Router>
+      )}
+    </Arwes>
+  );
 };
 
 export default App;
