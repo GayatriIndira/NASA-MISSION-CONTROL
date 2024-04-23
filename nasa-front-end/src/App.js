411e620e2821da9303baca01935b3fd6e596be3d
GayatriIndira <cgayatri@indiratrade.com>
2024-04-23 11:56:11 +0530

    build

--- nasa-front-end/src/App.js
+++ nasa-front-end/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Arwes, SoundsProvider, createSounds, createTheme } from "arwes";
+import { Arwes, SoundsProvider } from "arwes";
 
 import AppLayout from "./pages/AppLayout";
 import { resources, sounds } from "./settings";
 
-const theme = createTheme(); // Assuming you're creating a theme object
+// Define a basic theme object
+const theme = {
+  color: {
+    primary: "#00FF00", // Example primary color
+    // Define other theme colors here as needed
+  },
+  // Other theme properties can be added here
+};
 
 const App = () => {
   return (
-    <Arwes animate background={resources.background.large} pattern={resources.pattern}>
+    <Arwes animate background={resources.background.large} pattern={resources.pattern} theme={theme}>
       {(anim) => (
         <Router>
           <AppLayout show={anim.entered} />
